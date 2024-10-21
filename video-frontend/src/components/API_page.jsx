855236e5dab96e71ae184151683e147722f2afd6
tushar-glitch <tusharc20001@gmail.com>
2024-10-21 23:57:04 +0530

    added footer and improved api page

--- video-frontend/src/components/API_page.jsx
+++ video-frontend/src/components/API_page.jsx
@@ -1,6 +1,10 @@
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
-const API_page = () => <SwaggerUI url="/swagger-api-doc.json" />;
+const API_page = () => (
+  <div className="px-4 lg:px-20 ubuntu-light">
+    <SwaggerUI url="/swagger-api-doc.json" />
+  </div>
+);
 
 export default API_page;
--- video-frontend/src/components/Footer.jsx
+++ video-frontend/src/components/Footer.jsx
@@ -1,20 +1,27 @@
-import Footer from 'rc-footer';
-import 'rc-footer/assets/index.css'; // import 'rc-footer/asssets/index.less';
-import { render } from 'react-dom';
+import React from "react";
 
-render(
-  <Footer
-    columns={[
-      {
-        icon: (
-          <img src="https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg" />
-        ),
-        title: '语雀',
-        url: 'https://yuque.com',
-        description: '知识创作与分享工具',
-        openExternal: true,
-      },
-    ]}
-    bottom="Made with ❤️ by jumbo"
-  />,
-);
+const Footer = () => {
+  return (
+    <footer className="bg-zinc-50 text-center text-surface/75 dark:bg-neutral-700 dark:text-white/75 lg:text-left ubuntu-light">
+      <div className="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-white/10 lg:justify-between">
+        <div className="me-12 hidden lg:block">
+          <span>© 2024 VidCraft. All Rights Reserved.</span>
+        </div>
+        {/* Navigation links container */}
+        <div className="flex justify-center space-x-8">
+          <a href="#!" className="text-md">
+            About
+          </a>
+          <a href="#!" className="text-md">
+            Pricing
+          </a>
+          <a href="#!" className="text-md">
+            Contact
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
